5ee3df2ef0b8c130cf779f8f3b21806463fab37e
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-14 13:38:29 -0300

    Guarda da rota

--- gatitobook/src/app/autenticacao/autenticacao.guard.ts
+++ gatitobook/src/app/autenticacao/autenticacao.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AutenticacaoGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+export class AutenticacaoGuard implements CanLoad {
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.usuarioService.estaLogado()) {
+      this.router.navigate(['']);
+      return false;
+    }
+
     return true;
   }
-  
 }
