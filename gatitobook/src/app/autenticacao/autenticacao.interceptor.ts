7db5f95e51d747ab038ec44bb878397ff77da6a5
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-14 15:12:55 -0300

    Interceptor

--- gatitobook/src/app/autenticacao/autenticacao.interceptor.ts
+++ gatitobook/src/app/autenticacao/autenticacao.interceptor.ts
@@ -3,16 +3,26 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpHeaders,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable()
 export class AutenticacaoInterceptor implements HttpInterceptor {
+  constructor(private tokenService: TokenService) {}
 
-  constructor() {}
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (this.tokenService.possuiToken()) {
+      const token = this.tokenService.retornaToken();
+      const headers = new HttpHeaders().append('x-access-token', token);
+      request = request.clone({ headers });
+    }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request);
   }
 }
